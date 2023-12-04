619856743a6153982c739bbc4e52bf6423cf7a4d
SaraDrwish <38770404+SaraDrwish@users.noreply.github.com>
2023-12-04 22:11:27 +0300

    v - p1 - map 1

---
+++ proj1map/src/views/Home.vue
@@ -0,0 +1,12 @@
+<template>
+  <div class="home"><h1>Sara map vue web</h1></div>
+  <div class="flex flex-col h-screen max-h-screen text-pink-500"></div>
+</template>
+
+<script>
+export default {
+  name: "Home",
+};
+</script>
+
+<!-- //////// -->
